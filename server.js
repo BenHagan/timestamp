9fb4a4412d56d5125d597bf1fbf1770b8b5d82a1
Ben Hagan <benhagan@users.noreply.github.com>
2016-09-08 01:39:45 -0500

    Add homepage

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
-
+var path = require('path');
 app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
 
 app.get('/:time', function(req, res) {
   var time = req.params.time;
